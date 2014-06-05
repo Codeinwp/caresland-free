a49a3e0bdea19bc14a8e5e19f9d97c2cb2644a9e
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2014-06-05 15:22:23 +0300

    replaced hgroup with div added a check and if no logo is set do not output <img tag with an empty source.

--- header.php
+++ header.php
@@ -23,18 +23,23 @@
         <!-- [begin] header -->
         <header>
 			
-        	<div class="container">
-				<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+        	<div class="container">			
+				<?php $header_img_url = get_header_image(); ?>
+				
+				<?php if (!empty($header_img_url)) : ?>
+					<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+				<?php endif; ?>
+				
  				<div class="logo">
 					<?php if ( get_theme_mod( 'codeinwp_logo' ) ) : ?>
                         <div class='site-logo'>
                             <a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo get_theme_mod( 'codeinwp_logo' ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
                         </div>
                     <?php else : ?>
-                        <hgroup>
+                        <div> <!-- was hgroup -->
                             <h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
                             <h2 class='site-description'><?php bloginfo( 'description' ); ?></h2>
-                        </hgroup>
+                        </div>
                     <?php endif; ?>
                 </div><!-- .logo -->
 
