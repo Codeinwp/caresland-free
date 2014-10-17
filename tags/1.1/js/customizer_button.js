79d3833212fc90e572ea02299df5dc0d8f04ff79
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-10-17 16:27:41 -0500

    Sync with developemnt

--- tags/1.1/js/customizer_button.js
+++
@@ -1,3 +0,0 @@
-jQuery( document ).ready(function() {
-    jQuery('.wp-full-overlay-sidebar-content').prepend('<a href="http://themeisle.com/themes/caresland-pro/" class="button" target="_blank">View PRO version</a>');
-});
